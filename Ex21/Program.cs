0003f04a41a9737f25e5762103235773e0d6f025
DPrudnikov <dima1911@mail.ru>
2023-05-29 00:09:58 +0300

    Changed Ex21

--- Ex21/Program.cs
+++ Ex21/Program.cs
@@ -18,6 +18,6 @@ int y2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("z2 ");
 int z2 = Convert.ToInt32(Console.ReadLine());
 
-double R = (x2 - x1) + (y2 - y1) + (z2 - z1);
+double R = Math.Sqrt (Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2) + Math.Pow((x2-x1),2));
 
 Console.WriteLine(R);
