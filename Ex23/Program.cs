b6726a91ae642979bc444bb1d36b1c110752681a
DPrudnikov <dima1911@mail.ru>
2023-05-29 00:41:14 +0300

    Added Ex23

---
+++ Ex23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу кубов чисел  от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+double num = 0;
+int i = 0;
+
+while (i<N)
+{
+    Console.WriteLine(num = Math.Pow ((i + 1),3));
+    i = i + 1;  
+}
+Console.WriteLine("End");
